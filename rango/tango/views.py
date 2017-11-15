6294ca8deb38d5a130d01de449c301c9071dc4c3
Alex Becheru <becheru@gmail.com>
2017-11-15 16:52:46 +0200

    About page
    Added an about page, and link to index
    Added link to about page from index

--- rango/tango/views.py
+++ rango/tango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner")
+    return HttpResponse("""Rango says hey there partner. </br> Link to the <a href="http://localhost:8000/tango/about">about</a> page """)
+
+def about(request):
+    return HttpResponse("""Rango says this is the about page. </br> Link to the <a href="http://localhost:8000/tango/">main</a> page """)
