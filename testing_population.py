23e0b520c4904d6132673121b53f70b97cfae8e1
Alex Becheru <becheru@gmail.com>
2017-11-17 16:53:42 +0200

    Category update
    Add likes and views to each category
    Manually populate categories with data

--- testing_population.py
+++ testing_population.py
@@ -1,8 +1,4 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rango.settings')
-import django
-django.setup()
-from tango.models import Category, Page
 
 def populate():
     python_pages = [
@@ -33,35 +29,7 @@ def populate():
             }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
         for p in cat_data["pages"]:
-            add_page(c,p["title"],p["url"])
+            print(p["title"],p["url"])
 
-    for c in Category.objects.all():
-        for p in Page.objects.filter(category = c):
-            print(" - {0} - {1}".format(str(c),str(p)))
-
-def add_page(cat, title, url, views = 0):
-    p = Page.objects.get_or_create(category = cat, title = title)[0]
-    p.url = url
-    p.views = views
-    p.save()
-    return p
-
-def add_cat(name):
-    c = Category.objects.get_or_create(name = name)[0]
-    if name == "Python":
-        c.views = 128
-        c.likes = 64
-    elif name == "Django":
-        c.views = 64
-        c.likes = 32
-    elif name == "Frameworks":
-        c.views = 32
-        c.likes = 16
-    c.save()
-    return c
-
-if __name__ == '__main__':
-    print("Starting Tango population script...")
-    populate()
+populate()
