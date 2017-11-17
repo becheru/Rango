01a427c9c9d7b33a02897f05c72d500eddd76dca
Alex Becheru <becheru@gmail.com>
2017-11-17 12:29:44 +0200

    Add templates folder

--- rango/rango/settings.py
+++ rango/rango/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'rango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/Users/becheru/Projects/Rango/rango/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
