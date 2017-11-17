6407cc36da315bc55715c435c11e691500a55e58
Alex Becheru <becheru@gmail.com>
2017-11-17 14:02:39 +0200

    Static updates

--- rango/rango/settings.py
+++ rango/rango/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 STATIC_DIR = os.path.join(BASE_DIR, "static")
-STATICFILES_DIR = [STATIC_DIR, ]
+
 
 
 
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]
