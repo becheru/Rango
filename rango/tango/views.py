c95313142cf8a07040461aa02dc0ffafd4abbe3c
Alex Becheru <becheru@gmail.com>
2017-11-17 14:37:55 +0200

    Add media file to about page
    create about page template with variables
    add template for about page
    add media file to about page

--- rango/tango/views.py
+++ rango/tango/views.py
@@ -7,4 +7,6 @@ def index(request):
     #return HttpResponse("""Rango says hey there partner. </br> Link to the <a href="http://localhost:8000/tango/about">about</a> page """)
 
 def about(request):
-    return HttpResponse("""Rango says this is the about page. </br> Link to the <a href="http://localhost:8000/tango/">main</a> page """)
+    context_dict ={"var1": "This is var1", "var2": "This is var 2"}
+    return render(request, "tango/about.html", context = context_dict)
+    #return HttpResponse("""Rango says this is the about page. </br> Link to the <a href="http://localhost:8000/tango/">main</a> page """)
