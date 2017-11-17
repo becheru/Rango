3e6202f19b8416e9171b0c2e0fc9734effd08266
Alex Becheru <becheru@gmail.com>
2017-11-17 12:54:22 +0200

    Add index to view
    Modified intex view with html template

--- rango/tango/views.py
+++ rango/tango/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("""Rango says hey there partner. </br> Link to the <a href="http://localhost:8000/tango/about">about</a> page """)
+    context_dict = {"boldmessage" : "This is a message from Alex Becheru"}
+    return render(request, "tango/index.html", context = context_dict)
+    #return HttpResponse("""Rango says hey there partner. </br> Link to the <a href="http://localhost:8000/tango/about">about</a> page """)
 
 def about(request):
     return HttpResponse("""Rango says this is the about page. </br> Link to the <a href="http://localhost:8000/tango/">main</a> page """)
