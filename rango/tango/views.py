8d7435312e82fc1a5a5cbf1793b1b3cc7b0e7649
Alex Becheru <becheru@gmail.com>
2017-11-21 17:20:41 +0200

    Put the list of categories on the index

--- rango/tango/views.py
+++ rango/tango/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from tango.models import Category
 
 def index(request):
-    context_dict = {"boldmessage" : "This is a message from Alex Becheru"}
-    return render(request, "tango/index.html", context = context_dict)
+    #context_dict = {"boldmessage" : "This is a message from Alex Becheru"}
+    #return render(request, "tango/index.html", context = context_dict)
     #return HttpResponse("""Rango says hey there partner. </br> Link to the <a href="http://localhost:8000/tango/about">about</a> page """)
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = { "categories":category_list }
+    return render(request, "tango/index.html", context_dict)
 
 def about(request):
     context_dict ={"var1": "This is var1", "var2": "This is var 2"}
