9ade692c3b54594b383185d84849e98bfff53a9c
Alex Becheru <becheru@gmail.com>
2017-11-24 19:00:49 +0200

    Login added

--- rango/tango/views.py
+++ rango/tango/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
 from tango.models import Category
 from tango.models import Page
 from tango.models import UserProfile
@@ -91,3 +92,20 @@ def register(request):
         profile_form = UserProfileForm()
 
     return render(request, "tango/register.html", {"user_form": user_form, "profile_form": profile_form, 'registered': registered})
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username = username, password = password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your Tango account is disabled")
+        else:
+            print("Invalid login details: {0} {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied")
+    else:
+        return render(request, "tango/login.html", {})
